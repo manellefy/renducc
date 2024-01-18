57b9dd62ef2d06915743ff7bee9dc4772899890f
rahmakheriji <rahma.kheriji@ihec.ucar.tn>
2024-01-18 21:23:36 +0100

    first repo

---
+++ src/app/todo.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from './todo';
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  todoDetailShowModal=false
+  todoUpdateShowModal=false
+  todoAddShowModal=false
+  
+  
+  constructor(private httpCLient : HttpClient) { }
+
+  getTodoList():Observable<Todo[]>{
+    return this.httpCLient.get<Todo[]>("http://localhost:3000/todos")}
+  // on va retourner la liste de todo
+  
+  deleteTodo(todo:Todo):Observable<Todo[]>{
+  return this.httpCLient.delete<Todo[]>(`http://localhost:3000/todos/${todo.id}`)
+  }  
+  // on va retourner une liste de todos sauf l'element qu'on a effacer
+  getTodoByid(todo:Todo):Observable<Todo>{
+    return this.httpCLient.get<Todo>(`http://localhost:3000/todos/${todo.id}`) 
+    // on va retourner un seul element todo par son id
+    }
+  updateTodo(todo:Todo):Observable<Todo>{
+    return this.httpCLient.put<Todo>(`http://localhost:3000/todos/${todo.id}`,todo)
+  }
+  addTodo(todo:Todo):Observable<Todo[]>{
+    return this.httpCLient.post<Todo[]>(`http://localhost:3000/todos`,todo)
+  }
+}
